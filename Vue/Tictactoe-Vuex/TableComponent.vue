ab0454e3a4459d1a88962f62a66d3be769913d98
SangIl-fnino <mosang1230@naver.com>
2021-09-06 16:30:49 +0900

    TicTacToe - vuex, slot

---
+++ Vue/Tictactoe-Vuex/TableComponent.vue
@@ -0,0 +1,19 @@
+<template>
+  <table>
+    <slot />
+  </table>
+</template>
+
+<script>
+import TrComponent from "./TrComponent";
+
+export default {
+  components: {
+    TrComponent,
+  },
+  props: {},
+  computed: {},
+};
+</script>
+
+<style></style>
---
+++ Vue/Tictactoe-Vuex/TrComponent.vue
@@ -0,0 +1,27 @@
+<template>
+  <tr>
+    <td-component
+      v-for="(cellData, cellIndex) in rowData"
+      :key="cellIndex"
+      :cell-index="cellIndex"
+      :row-index="rowIndex"
+    ></td-component>
+  </tr>
+</template>
+
+<script>
+import TdComponent from "./TdComponent";
+export default {
+  components: {
+    TdComponent,
+  },
+  props: {
+    rowIndex: Number,
+  },
+  computed: {
+    rowData() {
+      return this.$store.state.tableData[this.rowIndex];
+    },
+  },
+};
+</script>
